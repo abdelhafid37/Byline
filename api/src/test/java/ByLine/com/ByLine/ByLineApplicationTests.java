e8c67493557b40a4a7b7011ac063845e0bbedf60
EL Mehdi Jessous <elmehdijessous01@gmail.com>
2026-04-22 20:36:12 +0100

    chore: Spring-boot intial
    config MYSQL + disable auto Security

---
+++ api/src/test/java/ByLine/com/ByLine/ByLineApplicationTests.java
@@ -0,0 +1,13 @@
+package ByLine.com.ByLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
